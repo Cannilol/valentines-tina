e44c00c006cf0489982cf32f77c090b51e41e344
Cannilol <151580449+Cannilol@users.noreply.github.com>
2025-02-01 18:57:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Really?",
+    "Baby pleasee?",
+    ":(",
+    "My love...",
+    "I'll be really sad if u say no",
+    "Tina please Im begging u",
+    "I will be very sad...",
+    "I will be very very very sad...",
+    "Why dont u want me",
+    "Say yes pleaseee ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
